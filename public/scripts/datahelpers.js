95b4553323b580b91a89fa57cceed7c41e556319
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-21 23:33:26 +0000

    Added helper function skeleton

---
+++ public/scripts/datahelpers.js
@@ -0,0 +1,27 @@
+// createNewUser
+
+// userAuthentication
+
+// showAllResources
+
+// populateResourceCards
+
+// getURLMetaData
+
+// filterResultsByCategory
+
+// filterResultsBySearchTerm
+
+// showUserResources
+
+// addNewResource
+
+// editResource
+
+// addResourceRating
+
+// addResourceComment
+
+// getUserData
+
+// updateUserData
