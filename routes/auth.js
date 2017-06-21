9ab1963ee07dd2c5e514fecafd5345496974844e
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-21 23:11:48 +0000

    Added route skeleton

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+// LOGIN - GET /login, POST /login
+// LOGOUT - GET /login, POST /login
+
+}
---
+++ routes/profile.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+// VIEW PROFILE = GET /user/:id
+// UPDATE PROFILE = POST /user/:id
+
+}
