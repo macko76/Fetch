a2bdfe7c5ad73d86f541485375a02d46478a9552
shinmike <shinmike90@gmail.com>
2017-06-23 22:22:04 +0000

    added form for user to add url, title, description about their new card

--- public/scripts/add-card.js
+++ public/scripts/add-card.js
@@ -1,8 +1,8 @@
 $(function(){
 
   $('.add-card').on('click', function(){
-    $('.row').append("<div class='row'></div>");
-    $('#searchText').focus();
+    $('.new-card').toggle();
+    $('.cardUrl').focus();
   });
 
 });
