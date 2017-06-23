ff1312a8e19e4390a1aad626de750bbaa3c22c09
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-23 07:19:10 +0000

    Finished EJS file restyling, added new partials for search and filter, plus switched login form to login buttons.

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, first_name: 'Rebecca', last_name: 'Haliburton', email: 'rebs@rebs.ca', password: 'rebs', profile_photo: 'http://placekitten.com/g/100/100'}),
-        knex('users').insert({id: 2, first_name: 'Mark', last_name: 'Bick', email: 'mark@mark.ca', password: 'mark', profile_photo: 'http://placekitten.com/g/100/100'}),
-        knex('users').insert({id: 3, first_name: 'Mike', last_name: 'Shin', email: 'mike@mike.ca', password: 'mike', profile_photo: 'http://placekitten.com/g/100/100'})
+        knex('users').insert({id: 1, first_name: 'Rebecca', last_name: 'Haliburton', email: 'rebs@rebs.ca', password: 'rebs', profile_photo: 'http://lorempixel.com/100/100/'}),
+        knex('users').insert({id: 2, first_name: 'Mark', last_name: 'Bick', email: 'mark@mark.ca', password: 'mark', profile_photo: 'http://lorempixel.com/100/100/'}),
+        knex('users').insert({id: 3, first_name: 'Mike', last_name: 'Shin', email: 'mike@mike.ca', password: 'mike', profile_photo: 'http://lorempixel.com/100/100/'})
       ]);
     });
 };
