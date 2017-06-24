c4fbdcf33d42a186667d0d57c9eeb1438f4ef5c7
shinmike <shinmike90@gmail.com>
2017-06-24 21:54:59 +0000

    cardUserId added

--- routes/resourcesModify.js
+++ routes/resourcesModify.js
@@ -11,13 +11,15 @@ module.exports = (knex) => {
     const cardImage = request.body.cardImage;
     const cardDescription = request.body.cardDescription;
     const cardCategory = request.body.cardCategory;
+    const cardUserId = request.session.userId;
 
     knex('resources')
       .insert({url: cardUrl, 
               image: cardImage, 
               title: cardTitle, 
               description: cardDescription,
-              category_id: cardCategory})
+              category_id: cardCategory,
+              user_id: cardUserId})
       .then((results) => {
         console.log("success!");
         response.json(results);
