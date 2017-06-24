ef7e7038d3011189f3b620f7eddda9d1181381de
shinmike <shinmike90@gmail.com>
2017-06-24 19:25:38 +0000

    fixing indentations

--- public/scripts/resourceFuncs.js
+++ public/scripts/resourceFuncs.js
@@ -14,18 +14,20 @@ function createResourceCard(resource) {
 
   return  `<div class="col-md-4">
             <div class="card">
-            <h1>${escape(title)}</h1>
-            <p class="card-title">${escape(title)}</p><img src="${escape(imageURL)}">
-            <p>${escape(description)}</p>
-            <div class="comment">Comment</div><fieldset class="rating">
-              <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-              <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-              <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-              <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-              <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label><br><br>
-             </fieldset>
-            <div id="comments-container"></div>
-          </div></div>`;
+              <h1>${escape(title)}</h1>
+              <p class="card-title">${escape(title)}</p><img src="${escape(imageURL)}">
+              <p>${escape(description)}</p>
+              <div class="comment">Comment</div>
+              <fieldset class="rating">
+                <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label><br><br>
+              </fieldset>
+              <div id="comments-container"></div>
+            </div>
+          </div>`;
 
 };
 
