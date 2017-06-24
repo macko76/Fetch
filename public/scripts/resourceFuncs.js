844dc67c0001d2fcda7c06d0597f59d8cbe11a12
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-24 18:55:05 +0000

    Fixed display function

--- public/scripts/resourceFuncs.js
+++ public/scripts/resourceFuncs.js
@@ -37,36 +37,36 @@ function createResourceCard(resource) {
 //   var imageURL = resource.url;
 
 //   return `<div class="col-md-4">
-//             <div class="card">
-            <div class="row">
-              <div class="col-md-10">
-                <p class="card-title">${escape(title)}</p>
-              </div>
-              <div class="col-md-2">
-                <button class="edit-button"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
-              </div>          
-//             <img src="${escape(imageURL)}">
-//             <p>${escape(description)}</p>
-//             <div class="comment">Comment</div><fieldset class="rating">
-//               <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-//               <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-//               <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-//               <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-//               <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label><br><br>
-//              </fieldset>
-//              <div id="comments-container"></div>
-//           </div></div>`;
+// //             <div class="card">
+//             <div class="row">
+//               <div class="col-md-10">
+//                 <p class="card-title">${escape(title)}</p>
+//               </div>
+//               <div class="col-md-2">
+//                 <button class="edit-button"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
+//               </div>          
+// //             <img src="${escape(imageURL)}">
+// //             <p>${escape(description)}</p>
+// //             <div class="comment">Comment</div><fieldset class="rating">
+// //               <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+// //               <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+// //               <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+// //               <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+// //               <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label><br><br>
+// //              </fieldset>
+// //              <div id="comments-container"></div>
+// //           </div></div>`;
 
-            <div class="edit-card-button">
-              <form action="users/resources/edit" method="POST" style="margin:150px;">
-                <input class="editCardUrl" type="text" name="editCardUrl" placeholder="${escape(imageURL)}" style="width:300px">
-                <input type="text" name="editCardTitle" style="width:300px" placeholder="${escape(title)}"><br>
-                <textarea name="editCardDescription" placeholder="${escape(description)}"></textarea>
-                <input type="submit" value="Submit">
-              </form>
-            </div>
+//             <div class="edit-card-button">
+//               <form action="users/resources/edit" method="POST" style="margin:150px;">
+//                 <input class="editCardUrl" type="text" name="editCardUrl" placeholder="${escape(imageURL)}" style="width:300px">
+//                 <input type="text" name="editCardTitle" style="width:300px" placeholder="${escape(title)}"><br>
+//                 <textarea name="editCardDescription" placeholder="${escape(description)}"></textarea>
+//                 <input type="submit" value="Submit">
+//               </form>
+//             </div>
             
-// };
+// // };
 
 // renderResources
 
