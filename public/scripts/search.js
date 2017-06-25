b09d3f5fdc0a0c4c07f5632362266150701e5905
Mark Bickford <mark.bickford@icloud.com>
2017-06-25 23:17:18 +0000

    Interim commit

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,13 +1,17 @@
 $(() => {
 
   $("#search-form").on('submit', function (event) {
-    event.preventDefault();
-    // const searchTerm = request.params;
-    $.ajax({
-      method: "GET",
-      url: `/api/resources/search`
-    }).done((resources) => {
-      renderUserResources(resources);
-    });
+    // event.preventDefault();
+    const searchText = req.params;
+    console.log(searchText);
+    alert("newest!!!");
+    // event.preventDefault();
+    // // const searchTerm = request.params;
+    // $.ajax({
+    //   method: "GET",
+    //   url: `/api/resources/search`
+    // }).done((resources) => {
+    //   renderUserResources(resources);
+    // });
   });
 });
